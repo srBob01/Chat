2c1693c2f1d871ad8d44e596c240f2163eac7325
srBob01 <necit717@gmail.com>
2023-11-04 20:31:38 +0300

    add skeleton

---
+++ ChatProject/FitnessClub.WebAPI/Settings/ChatSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Chat.WebAPI.Settings
+{
+    public class FitnessClubSettings
+    {
+        //здесь будут приложения, например, строка подключения к бд
+    }
+}
---
+++ ChatProject/FitnessClub.WebAPI/Settings/ChatSettingsReader.cs
@@ -0,0 +1,11 @@
+﻿namespace Chat.WebAPI.Settings
+{
+    public static class ChatSettingsReader
+    {
+        public static ChatSettings Read(IConfiguration configuration)
+        {
+            //здесь будет чтение настроек приложения из конфига
+            return new ChatSettings();
+        }
+    }
+}
